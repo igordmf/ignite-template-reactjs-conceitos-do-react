d2f6852b769d595450a6acf433d02fdb0b38c734
Igor Farias <igor.farias@liferay.com>
2021-11-12 15:07:59 -0300

    Finaliza desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,28 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === '') return;
+    const newTask = {
+      id: tasks[tasks.length - 1]?.id ? tasks[tasks.length - 1].id + 1 : 1,
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) return { ...task, isComplete: !task.isComplete};
+      return task;
+    })
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
   }
 
   return (
@@ -66,7 +80,7 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
